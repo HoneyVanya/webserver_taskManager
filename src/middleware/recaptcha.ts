bbe55cdb561a9b5dec4a6eeb8b0f9e30a043898b
HoneyVanya <bad.batareika@gmail.com>
2025-08-23 18:33:04 +0200

    Refactored recapthca.ts, added logger with pino and response variable

--- src/middleware/recaptcha.ts
+++ src/middleware/recaptcha.ts
@@ -1,7 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, response } from 'express';
 import asyncHandler from 'express-async-handler';
 import axios from 'axios';
 import { env } from '../config/env.js';
+import logger from '../config/logger.js';
 
 export const verifyRecaptcha = asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
@@ -14,17 +15,33 @@ export const verifyRecaptcha = asyncHandler(
 
         try {
             const response = await axios.post(
-                `https://www.google.com/recaptcha/api/siteverify?secret=${env.RECAPTCHA_SECRET_KEY}&response=${recaptchaToken}`
+                'https://www.google.com/recaptcha/api/siteverify',
+                null,
+                {
+                    params: {
+                        secret: env.RECAPTCHA_SECRET_KEY,
+                        response: recaptchaToken,
+                    },
+                }
             );
 
-            if (response.data.success) {
+            if (response.data.success === true) {
                 next();
             } else {
+                logger.warn(
+                    { recaptchaErrors: response.data['error-codes'] },
+                    'ReCaptcha verification failed'
+                );
                 res.status(401);
                 throw new Error('Failed ReCaptcha verification.');
             }
         } catch (error) {
-            throw new Error('Error verifying ReCaptcha.');
+            logger.error(
+                error,
+                'Catastrofic error during Recaptcha verification.'
+            );
+            res.status(500);
+            throw new Error('Error verifying ReCaptcha with Google`.');
         }
     }
 );
