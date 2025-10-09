4c50bb2c06ad81845c02eb3334f3b10ce4ca80de
HoneyVanya <bad.batareika@gmail.com>
2025-10-09 19:54:40 +0200

    simplified auth.schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,16 +1,12 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-    body: z.object({
-        email: z.string({ required_error: 'Email is required' }).email(),
-        password: z.string({ required_error: 'Password is required' }),
-    }),
+    email: z.string({ required_error: 'Email is required' }).email(),
+    password: z.string({ required_error: 'Password is required' }),
 });
 
 export const refreshTokenSchema = z.object({
-    body: z.object({
-        refreshToken: z.string({
-            required_error: 'Refresh token is required',
-        }),
+    refreshToken: z.string({
+        required_error: 'Refresh token is required',
     }),
 });
